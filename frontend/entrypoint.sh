f416a8c35130d7bfce155ceabf2b021043ca6e4c
koh884minimini <64010546+koh884minimini@users.noreply.github.com>
2025-09-28 03:57:28 +0000

    f

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 # frontend/entrypoint.sh
-
-# --- バックエンドAPIのURL設定 ---
-# このURLは、Codespacesを起動するたびに変わる可能性があります。
-# 起動後、Codespacesの「ポート」タブでバックエンド(3001番)の
-# 「転送済みのアドレス」をコピーし、以下のURLに貼り付けてください。
 export REACT_APP_API_BASE_URL='https://YOUR_CODESPACES_BACKEND_URL_HERE-3001.app.github.dev'
-
-# Reactの開発サーバーを起動
 exec npm start
